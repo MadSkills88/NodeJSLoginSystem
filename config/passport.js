6a107d20326dc22ff7c075c8b55b732a9105356e
Anthony Bao <anthonybao1999@gmail.com>
2016-03-08 15:43:10 -0700

    d

--- config/passport.js
+++ config/passport.js
@@ -182,7 +182,7 @@ module.exports = function(passport) {
                             user.facebook.token = token;
                             user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
                             user.facebook.email = (profile.emails[0].value || '').toLowerCase();
-                            user.facebook.picture = profile.photos[1].value;
+                            user.facebook.picture = profile.photos[0].value + '?type=large';
                             user.save(function(err) {
                                 if (err)
                                     return done(err);
@@ -199,7 +199,7 @@ module.exports = function(passport) {
                         newUser.facebook.token = token;
                         newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
                         newUser.facebook.email = (profile.emails[0].value || '').toLowerCase();
-                        newUser.facebook.picture = profile.photos[1].value;
+                        newUser.facebook.picture = profile.photos[0].value + '?type=large';
                         newUser.save(function(err) {
                             if (err)
                                 return done(err);
@@ -217,7 +217,7 @@ module.exports = function(passport) {
                 user.facebook.token = token;
                 user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
                 user.facebook.email = (profile.emails[0].value || '').toLowerCase();
-                user.facebook.picture = profile.photos[1].value;
+                user.facebook.picture = profile.photos[0].value + '?type=large';
                 user.save(function(err) {
                     if (err)
                         return done(err);
